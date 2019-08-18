1a371d25456a58bc7f5f497a4dbd21eef921f9c8
fluffy <fluffy@yujin.cloud>
2019-08-18 03:28:58 -0400

    spring boot framework app

---
+++ java/src/main/java/com/h2x/mvp/H2xmvpApplication.java
@@ -0,0 +1,13 @@
+package com.h2x.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2xmvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2xmvpApplication.class, args);
+    }
+
+}
