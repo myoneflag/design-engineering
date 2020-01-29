695e85d59e684acadd69631766f4666c4eacdd9e
fluffy <fluffy@yujin.cloud>
2020-01-30 08:05:36 +1100

    oopsies

--- node/common/src/api/document/entities/types.ts
+++ node/common/src/api/document/entities/types.ts
@@ -1,3 +1,6 @@
+import { DrawableEntityConcrete } from "./concrete-entity";
+import { assertUnreachable } from "../../config";
+
 export enum EntityType {
     BACKGROUND_IMAGE = "BACKGROUND_IMAGE",
     RISER = "RISER",
@@ -44,5 +47,28 @@ export function getEntityName(type: EntityType): string {
         case EntityType.FLOW_SOURCE:
             return "Flow Source";
     }
+    assertUnreachable(type);
 }
 
+export function getReferences(entity: DrawableEntityConcrete): string[] {
+    switch (entity.type) {
+        case EntityType.BACKGROUND_IMAGE:
+        case EntityType.SYSTEM_NODE:
+        case EntityType.RISER:
+        case EntityType.LOAD_NODE:
+        case EntityType.FITTING:
+        case EntityType.FLOW_SOURCE:
+            return [];
+        case EntityType.PIPE:
+            return [entity.endpointUid[0], entity.endpointUid[1]];
+        case EntityType.BIG_VALVE:
+            return [entity.coldRoughInUid, entity.hotRoughInUid];
+        case EntityType.FIXTURE:
+            return entity.roughInsInOrder.map((r) => entity.roughIns[r].uid);
+        case EntityType.DIRECTED_VALVE:
+            return [entity.sourceUid];
+        case EntityType.PLANT:
+            return [entity.inletUid, entity.outletUid];
+    }
+    assertUnreachable(entity);
+}
