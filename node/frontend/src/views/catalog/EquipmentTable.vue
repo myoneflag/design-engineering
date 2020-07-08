e636515fee8a128ff487e81c20ae3b957d87f87c
Max Wu <mxwu1994@gmail.com>
2020-07-08 16:33:55 +1000

    Table UIs in budget

---
+++ node/frontend/src/views/catalog/EquipmentTable.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <template v-for="component in components">
+            <b-row>
+                <b-col>
+                    <h4>{{valve}}</h4>
+                </b-col>
+            </b-row>
+            <b-table
+                    small
+                    striped
+                    :items="items(component)"
+                    :fields="fields"
+                    style="max-width: 100%; overflow-x: auto; margin-top:25px"
+                    responsive="true"
+            >
+                <template v-slot:[cellKey]="slot" >
+                    <b-input-group prepend="$" size="sm">
+                        <b-form-input
+                                size="sm"
+                                type="number"
+                                @input="(e) => onCellInput(valve, slot.item['Size (mm)'], e)"
+                                :value="slot.value"
+                        ></b-form-input>
+                    </b-input-group>
+                </template>
+            </b-table>
+            <br/>
+            <br/>
+        </template>
+    </div>
+</template>
+<script lang="ts">
+    import Component from "vue-class-component";
+    import Vue from "vue";
+    import {
+        PipesBySize,
+        PipesTable as PipesTableType,
+        ValvesTable as ValvesTableType,
+        PriceTable,
+    } from "../../../../common/src/api/catalog/price-table";
+    import {defaultPriceTable} from "../../../../common/src/api/catalog/default-price-table";
+
+    @Component({
+        components: { },
+        props: {
+        }
+    })
+    export default class ValvesTable extends Vue {
+        get priceTable(): PriceTable {
+            return defaultPriceTable;
+        }
+
+        get components() {
+            return Object.keys(this.priceTable.Components);
+        }
+
+        get fields() {
+            return ["Size (mm)", "Unit cost"];
+        }
+
+        get cellKey() {
+            return 'cell(Unit cost)';
+        }
+
+        get materials() {
+            return Object.keys(this.priceTable.Pipes);
+        }
+
+        onCellInput(valve: keyof ValvesTableType, size: number, value: number) {
+            defaultPriceTable.Valves[valve][size] = Number(value);
+        }
+
+        items(valve: keyof ValvesTableType) {
+            const itemsBySize: {[key: number]: any} = {};
+            for (const [size, cost] of Object.entries(this.priceTable.Valves[valve])) {
+                itemsBySize[Number(size)] = {"Size (mm)": size, "Unit cost": cost};
+            }
+            return Object.keys(itemsBySize).sort((a, b) => Number(a) - Number(b)).map((size) => itemsBySize[Number(size)]);
+        }
+    }
+</script>
+<style lang="less"></style>
