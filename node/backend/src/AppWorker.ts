7dc033f2b186b904f5c11bd852150d350ed1af15
Calin Pupaza <calin@h2xengineering.com>
2021-04-09 16:56:46 -0700

    Remove unused mq and ws config for worker

--- node/backend/src/AppWorker.ts
+++ node/backend/src/AppWorker.ts
@@ -1,71 +1,24 @@
 import bodyParser from "body-parser";
 import cors from "cors";
-import helmet from "helmet";
 import morgan from "morgan";
 import express from "express";
-import expressWs from "express-ws"; // Avoid import rearrangement in this file - express-ws needs to be after express.
-import CONFIG from './config/config';
-
-// for StompJS
-Object.assign(global, { WebSocket: require('ws') });
-
-const expressApp = expressWs(express(), undefined, {
-    wsOptions: {
-        perMessageDeflate: {
-            zlibDeflateOptions: {
-                // See zlib defaults.
-                chunkSize: 1024,
-                memLevel: 7,
-                level: 3
-            },
-            zlibInflateOptions: {
-                chunkSize: 10 * 1024
-            },
-            // Other options settable:
-            clientNoContextTakeover: true, // Defaults to negotiated value.
-            serverNoContextTakeover: true, // Defaults to negotiated value.
-            serverMaxWindowBits: 10, // Defaults to negotiated value.
-            // Below options specified as default values.
-            concurrencyLimit: 10, // Limits zlib concurrency for perf.
-            threshold: 1024 // Size (in bytes) below which messages
-            // should not be compressed.
-        }
-    }
-}).app;
-
 import routerWorker from "./router-worker";
 import * as errorHandler from "./helpers/errorHandler";
-import * as path from "path";
-import MqClient from "./services/MqClient";
-import { DocumentUpgrader } from "./services/DocumentUpgrader";
 
 class AppWorker {
-    public express: expressWs.Application;
+    public express: express.Application;
 
     constructor() {
-        this.express = expressApp;
+        this.express = express();
         this.setMiddlewares();
-        this.setWebsockets();
-        this.setMqServices();
         this.setRoutes();
         this.catchErrors();
-
-    }
-
-    private setWebsockets() {
-        // No op
-    }
-
-    private setMqServices() {
-        MqClient.activate();
     }
 
     private setMiddlewares(): void {
         this.express.use(cors());
         this.express.use(morgan("dev"));
         this.express.use(bodyParser.json());
-        this.express.use(bodyParser.urlencoded({ extended: false }));
-        this.express.use(helmet());
     }
 
     private setRoutes(): void {
