da023bc06d8360575d1d1aedc1ff7f401acac0ae
Calin Pupaza <calin@h2xengineering.com>
2021-04-16 20:23:25 -0700

    Adding DocumentUpgrader calls to worker

--- node/backend/src/controllers/worker.ts
+++ node/backend/src/controllers/worker.ts
@@ -1,11 +1,26 @@
 
 import { Request, Response, Router } from "express";
+import { DocumentUpgrader, Tasks } from "../services/DocumentUpgrader";
 
 export class WorkerController {
     public async processQueueMessage(req: Request, res: Response) {
         const jsonMessage = req.body;
         console.log("Worker Message Received")
         console.log(jsonMessage)
+
+        switch (jsonMessage.task) {
+            case Tasks.DocumentUpgradeScan:
+                await DocumentUpgrader.submitDocumentsForUpgrade()
+                break
+            case Tasks.DocumentUpgradeExecute:
+                let docId: number;
+                docId = req.body.parameters.docId
+                await DocumentUpgrader.onDocumentUpgradeRequest(docId);
+                break;
+            default:
+                console.log("unknown message type")
+        }
+
         return res.status(200).send({
             success: true
         });        
