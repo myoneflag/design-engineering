55bee9c4215ddb2824ee346341ce47d09d552060
Calin Pupaza <calin@h2xengineering.com>
2021-04-21 11:52:45 -0700

    Added new commands for docker

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 export EnvName=stage
-pushd docker && npm run login && npm run build-worker && npm run publish && popd
+pushd docker && npm run login && npm run build:all --tagret=prod && npm run publish && popd
 pushd cloudformation && npm run update && popd
