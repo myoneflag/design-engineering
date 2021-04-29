0dd5da82954010017e7fce1814812f864df5a2a7
Calin Pupaza <calin@h2xengineering.com>
2021-04-28 21:04:03 -0700

    RDS IAM user auth

--- node/ormconfig.js
+++ node/ormconfig.js
@@ -1,24 +1,44 @@
+const AWS = require("aws-sdk");
+
+const type = "postgres"
+const host = process.env.RDS_HOSTNAME || process.env.DB_HOSTNAME || "db"
+const port = process.env.RDS_PORT || process.env.DB_PORT || 5432
+const username = process.env.RDS_USERNAME || process.env.DB_USERNAME || "postgres"
+const database = process.env.RDS_DB_NAME || process.env.DB_NAME || "h2x"
+const password = process.env.RDS_PASSWORD || process.env.DB_PASSWORD || "postgres"
+
 module.exports = {
-  "type": "postgres",
-  "host":  process.env.RDS_HOSTNAME || process.env.DB_HOSTNAME || (process.env.MODE === "production" ? "db" : "db"),
-  "port": process.env.RDS_PORT || process.env.DB_PORT || 5432,
-  "username": process.env.RDS_USERNAME || process.env.DB_USERNAME || "postgres",
-  "password": process.env.RDS_PASSWORD || process.env.DB_PASSWORD || "postgres",
-  "database": process.env.RDS_DB_NAME || process.env.DB_NAME || "h2x",
-  "synchronize": false,
-  "logging": process.env.MODE === "production" ? false : true,
-  "entities": [
+  type,
+  host,
+  port,
+  username,
+  password: async () => {
+    if (!password) {
+      const signer = new AWS.RDS.Signer({
+        region: (new AWS.Config()).region,
+        hostname: host,
+        port: port,
+        username: username,
+      });
+      return signer.getAuthToken({});
+    }
+    return password;
+  },
+  database,
+  synchronize: false,
+  logging: process.env.MODE === "production" ? false : true,
+  entities: [
     process.env.MODE === "production" ? "./dist/common/src/models/**/*{.ts,.js}" : "../common/src/models/**/*.ts"
   ],
-  "migrations": [
+  migrations: [
     process.env.MODE === "production" ? "./dist/backend/src/migration/**/*{.ts,.js}" : "./src/migration/**/*.ts"
   ],
-  "migrationsRun": true,
-  "migrationsTableName": "migrations",
-  "subscribers": [
+  migrationsRun: true,
+  migrationsTableName: "migrations",
+  subscribers: [
     "./src/subscriber/**/*.ts"
   ],
-  "cli": {
+  cli: {
     "entitiesDir": "../common/src/models",
     "migrationsDir": "./src/migration",
     "subscribersDir": "./src/subscriber"
