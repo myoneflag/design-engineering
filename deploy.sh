73037acf72932ba94d1d9c93e89072fba1cbf219
Calin Pupaza <calin@h2xengineering.com>
2021-04-30 08:37:46 -0700

    Adding conditional to create or update an existing stack

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
-pushd docker && npm run login && npm run build:all --target=prod && npm run publish && popd
-pushd cloudformation && npm run update && popd
+# pushd docker && npm run login && npm run build:all --target=prod && npm run publish && popd
+pushd cloudformation
+if [[ $(npm run stack-exists) == *false ]]; then
+    npm run create
+else
+    npm run update
+fi
+popd
