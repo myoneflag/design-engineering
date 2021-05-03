333a336fecdd753b6f3a94a8f0a0a5ba0b0632a3
Calin Pupaza <calin@h2xengineering.com>
2021-05-03 15:38:19 -0700

    Conservative transaction timeout and retry settings

--- node/backend/src/helpers/database.ts
+++ node/backend/src/helpers/database.ts
@@ -4,20 +4,21 @@ import retry from "retry";
 export function withSerializableTransaction<T>(retryable: (tx: EntityManager) => Promise<T>) {
     return new Promise<any>((res, rej) => {
         // randomize so that conflicting transactions don't just cancel each other over and over.
-        const operation = retry.operation({randomize: true, minTimeout: 200, forever: true});
+        const operation = retry.operation({randomize: true, minTimeout: 200, maxTimeout: 1000, retries: 10});
 
         operation.attempt(async () => {
             try {
                 await getManager().transaction('SERIALIZABLE', async (tx) => {
+                    console.debug('attemptTransaction', tx)                    
                     res(await retryable(tx));
                 });
             } catch (e) {
+                console.warn('tentativeTransactionFail', e)
                 if (operation.retry(e)) {
-                    console.warn('retrying transaction');
-                    console.trace();
+                    console.warn('retryTransaction', e);
                     return;
                 } else {
-                    console.warn('cannot retry transaction: failed completely and gave up');
+                    console.error('transactionFinalError', e)
                     rej(e);
                 }
             }
