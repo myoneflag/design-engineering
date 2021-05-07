57f978d88030ad2483152cfceaf0d1361776c1fe
Calin Pupaza <calin@h2xengineering.com>
2021-05-07 08:56:41 -0700

    Removed AutoCAD and adding link to plugin manual

--- node/frontend/src/components/editor/CalculationsSidebar.vue
+++ node/frontend/src/components/editor/CalculationsSidebar.vue
@@ -20,11 +20,8 @@
                         <b-dropdown-item @click="budgetReport" variant="outline-dark" size="sm"> 
                             Bill of Materials (.xlsx)
                         </b-dropdown-item>
-                        <b-dropdown-item variant="outline-dark" size="sm" :disabled="true">
-                            AutoCAD (.dwg) (Coming soon)
-                        </b-dropdown-item>
                         <b-dropdown-item @click="jsonExport" variant="outline-dark" size="sm">
-                            Revit Plugin (.json) <b-badge>BETA</b-badge> &nbsp; <b-link v-on:click.stop href="https://h2xengineering.com" target="_blank"><b-icon icon="info-square" variant="dark"></b-icon></b-link>
+                            Revit Plugin (.json) <b-badge>BETA</b-badge> &nbsp; <b-link v-on:click.stop href="https://docs.google.com/presentation/d/1ovpnRS5sYKGYCy8pQClG2okuyqdG81gYSzH5Nmiut8U/edit?usp=sharing" target="_blank"><b-icon icon="info-square" variant="dark"></b-icon></b-link>
                         </b-dropdown-item>
                     </b-dropdown>
                     <b-button
