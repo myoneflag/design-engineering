dc71db166879f753cb72d38132608e33964bbad9
Calin Pupaza <calin@h2xengineering.com>
2021-05-10 15:52:30 -0700

    Fix merge conflict

--- node/backend/src/helpers/database.ts
+++ node/backend/src/helpers/database.ts
@@ -1,14 +1,20 @@
 import { EntityManager, getManager } from "typeorm";
 import retry from "retry";
+import {IsolationLevel} from "typeorm/driver/types/IsolationLevel";
 
 export function withSerializableTransaction<T>(retryable: (tx: EntityManager) => Promise<T>) {
+    return withTransaction('SERIALIZABLE', retryable);
+}
+
+export function withTransaction<T>(isolationLevel: IsolationLevel, retryable: (tx: EntityManager) => Promise<T>) {
     return new Promise<any>((res, rej) => {
         // randomize so that conflicting transactions don't just cancel each other over and over.
         const operation = retry.operation({randomize: true, minTimeout: 200, maxTimeout: 1000, retries: 10});
 
         operation.attempt(async () => {
             try {
-                await getManager().transaction('SERIALIZABLE', async (tx) => {
+                console.log('transactionStart', isolationLevel)
+                await getManager().transaction(isolationLevel, async (tx) => {
                     console.debug('attemptTransaction', tx)                    
                     res(await retryable(tx));
                 });
