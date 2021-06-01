7134751a15bf39be5e7cfcf420eb1375d1a7fff5
Calin Pupaza <calin@h2xengineering.com>
2021-06-01 15:24:47 -0700

    [upgrade] No retry for transactions

--- node/backend/src/helpers/database.ts
+++ node/backend/src/helpers/database.ts
@@ -1,5 +1,4 @@
 import { EntityManager, getManager } from "typeorm";
-import retry from "retry";
 import {IsolationLevel} from "typeorm/driver/types/IsolationLevel";
 
 export function withSerializableTransaction<T>(retryable: (tx: EntityManager) => Promise<T>) {
@@ -7,27 +6,17 @@ export function withSerializableTransaction<T>(retryable: (tx: EntityManager) =>
 }
 
 export function withTransaction<T>(isolationLevel: IsolationLevel, retryable: (tx: EntityManager) => Promise<T>) {
-    return new Promise<any>((res, rej) => {
-        // randomize so that conflicting transactions don't just cancel each other over and over.
-        const operation = retry.operation({randomize: true, minTimeout: 200, maxTimeout: 1000, retries: 10});
-
-        operation.attempt(async () => {
+    const txid = Date.now()    
+    return new Promise<any>((res, rej) => {    
+        return getManager().transaction(isolationLevel, async (tx) => {
+            console.log('transactionStart', `txid=${txid}`, isolationLevel)
             try {
-                console.log('transactionStart', isolationLevel)
-                await getManager().transaction(isolationLevel, async (tx) => {
-                    console.debug('attemptTransaction', tx)                    
-                    res(await retryable(tx));
-                });
+                let result = await retryable(tx);
+                res(result)
             } catch (e) {
-                console.warn('tentativeTransactionFail', e)
-                if (operation.retry(e)) {
-                    console.warn('retryTransaction', e);
-                    return;
-                } else {
-                    console.error('transactionFinalError', e)
-                    rej(e);
-                }
+                console.warn('transactionError', `txid=${txid}`, e)
+                rej(e)                
             }
         });
-    });
+    })
 }
