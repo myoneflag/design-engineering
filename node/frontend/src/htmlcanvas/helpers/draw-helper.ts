4d099d44c114dab12847f1587dea0f49b2a99c13
saman teymoori <43384880+samanteymoori@users.noreply.github.com>
2021-08-13 16:32:16 +0430

    W-320 warning fix + pipe cap+drainage / pressure

--- node/frontend/src/htmlcanvas/helpers/draw-helper.ts
+++ node/frontend/src/htmlcanvas/helpers/draw-helper.ts
@@ -4,24 +4,40 @@ export function drawPipeCap(ctx: CanvasRenderingContext2D, point: Point, side: S
         const base:number=50;
         const rectangles: Rectangle[] = [
             {
-                point: point,
-                width: base / 10,
-                height: base,
+                point:
+                    side === Side.TOP
+                        ? { top: point.top - base / 2, left: point.left - base / 2 }
+                        : side === Side.BOTTOM
+                        ? { top: point.top + base / 2, left: point.left - base / 2 }
+                        : point,
+                width: side === Side.TOP || side === Side.BOTTOM ? base : base / 10,
+                height: side === Side.TOP || side === Side.BOTTOM ? base / 10 : base,
                 strokeStyle
             },
             {
-                point: side === Side.LEFT ? point : { top: point.top, left: point.left - base/2 },
-                width: base / 2,
-                height: base / 10,
+                point:
+                    side === Side.LEFT
+                        ? point
+                        : side === Side.TOP
+                        ? { top: point.top - base / 2, left: point.left - base / 2 }
+                        : side === Side.BOTTOM
+                        ? { top: point.top, left: point.left - base / 2 }
+                        : { top: point.top, left: point.left - base / 2 },
+                width: side === Side.TOP || side === Side.BOTTOM ? base / 10 : base / 2,
+                height: side === Side.TOP || side === Side.BOTTOM ? base / 2 : base / 10,
                 strokeStyle
             },
             {
                 point:
                     side === Side.LEFT
                         ? { top: point.top + base, left: point.left }
+                        : side === Side.TOP
+                        ? { top: point.top - base / 2, left: point.left - base / 2 + base }
+                        : side === Side.BOTTOM
+                        ? { top: point.top, left: point.left - base / 2 + base }
                         : { top: point.top + base, left: point.left - base / 2 },
-                width: base / 2,
-                height: base / 10,
+                width: side === Side.TOP || side === Side.BOTTOM ? base / 10 : base / 2,
+                height: side === Side.TOP || side === Side.BOTTOM ? base / 2 : base / 10,
                 strokeStyle
             }
         ];
