81279cec372f915bdbee1edcbc7cd0b381798aca
Calin Pupaza <calin@h2xengineering.com>
2021-10-14 13:16:28 +0000

    Fix Vue hot reload - Updated config and script

--- node/frontend/vue.config.js
+++ node/frontend/vue.config.js
@@ -1,24 +1,30 @@
-var webpack = require('webpack');
+var webpack = require("webpack");
 
 module.exports = {
-    productionSourceMap: true,
-    configureWebpack: {
-        devServer: {
-            port: 80
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.env': {
-                    PACKAGE_JSON: '"' + escape(JSON.stringify(require('./package.json'))) + '"'
-                },
-            }),
-        ],
+  productionSourceMap: true,
+  configureWebpack: {
+    devServer: {
+      port: 80,
+      // Uncomment below in case Vue Hotreload is not working
+      // Might happen if you are on Windows or FSEvents on Mac are captured by another program
+      // https://github.com/vuejs-templates/webpack/issues/378
+      watchOptions: {
+        poll: true
+      }
     },
-    pwa: {
-        workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-            swSrc: 'service-worker.js',
-        },
-    },
-// the rest of your original module.exports code goes here
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": {
+          PACKAGE_JSON: '"' + escape(JSON.stringify(require("./package.json"))) + '"'
+        }
+      })
+    ]
+  },
+  pwa: {
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "service-worker.js"
+    }
+  }
+  // the rest of your original module.exports code goes here
 };
