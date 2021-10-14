072b8cdf88b4d74b413b2e8188e208aa37a767ed
Calin Pupaza <calin@h2xengineering.com>
2021-10-14 13:16:28 +0000

    Fix Vue hot reload - Updated config and script

---
+++ vetur.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  settings: {
+    "vetur.useWorkspaceDependencies": true,
+    "vetur.experimental.templateInterpolationService": true,
+  },
+  projects: ["./node/frontend", "./node/common", "./node/backend"],
+};
