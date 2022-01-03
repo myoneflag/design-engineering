2d75f9fe00e5bfe7a3fedb96b72be7ae314133ba
Calin Pupaza <calin@h2xengineering.com>
2022-01-03 01:12:19 +0000

    DEV-523 Delete S3 buckets when deleting environment

--- cloudformation/delete.sh
+++ cloudformation/delete.sh
@@ -1,12 +1,18 @@
 set -e
 
-cd cloudformation
-npm install
-if [ "$(npm run --silent stack-exists)" == "false" ]
-then
-    echo "Stack does not exist"
+if [ $env == "prod" ] || [ $env == "stage" ] || [ $env == "test" ]
+then    
+    echo $env "environment cannot be delete"
 else
-    echo "Stack will be deleted"
-    npm run delete
+    echo $env "stack will be deleted"
+    cd cloudformation
+    npm install
+    if [ "$(npm run --silent stack-exists)" == "false" ]
+    then
+        echo "Stack does not exist"
+    else
+        echo "Stack will be deleted"
+        npm run delete
+    fi
+    cd ..
 fi
-cd ..
